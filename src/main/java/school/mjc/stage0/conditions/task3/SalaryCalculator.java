48ba95bb42fa2090860f0f631d77407bf3c7be29
AlokhinaTetiana <aliokhinatania03@gmail.com>
2023-06-26 14:29:57 +0300

    g

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task3;
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
         if(0<salary && salary<=10000){System.out.println(salary*(1-0.15));}
-        else if (10000<salary && salary<=20000){System.out.println(salary*(1-0.18));}
+        else if (10000<salary && salary<=20000){System.out.println(salary-0.18*salary);}
         else if (salary>20000){System.out.println(salary*(1-0.2));}
         else{System.out.println("wrong input!");}
     }
